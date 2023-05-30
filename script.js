cb73a5551f772bb749ed2be64871da353e8aaf7f
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-30 22:11:45 +0300

    New

--- script.js
+++ script.js
@@ -143,6 +143,7 @@ function startTheGame() {
             newRandomAsteroid(listCoordObj, listXAxesObj, listYAxesObj, initWidth, counter);
         } else if (listYAxesObj.val[1] == 575) {
             console.log("True 2");
+            console.log(listXAxesObj.val[3] + " " + listYAxesObj.val[3]);
             console.log(listXAxesObj.val[2] + " " + listYAxesObj.val[2]);
             console.log(listXAxesObj.val[1] + " " + listYAxesObj.val[1]);
             changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 2);
@@ -152,6 +153,8 @@ function startTheGame() {
             newRandomAsteroid(listCoordObj, listXAxesObj, listYAxesObj, initWidth, counter);
         } else if (listYAxesObj.val[0] == 300) {
             console.log("True 1");
+            console.log(listXAxesObj.val[3] + " " + listYAxesObj.val[3]);
+            console.log(listXAxesObj.val[2] + " " + listYAxesObj.val[2]);
             console.log(listXAxesObj.val[1] + " " + listYAxesObj.val[1]);
             changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 1);
             // changeCoordonates(width2, height2, width1, height1, currentValue2, currentValue1);
