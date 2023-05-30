f8dfef2b3212f2cf5570f4acca24ac6fee215bfe
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-30 22:14:05 +0300

    New

--- script.js
+++ script.js
@@ -134,6 +134,7 @@ function startTheGame() {
             console.log(listXAxesObj.val[3] + " " + listYAxesObj.val[3]);
             console.log(listXAxesObj.val[2] + " " + listYAxesObj.val[2]);
             console.log(listXAxesObj.val[1] + " " + listYAxesObj.val[1]);
+            console.log(listXAxesObj.val[0] + " " + listYAxesObj.val[0]);
             changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 3);
             //changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 2);
             //changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 1);
@@ -146,6 +147,7 @@ function startTheGame() {
             console.log(listXAxesObj.val[3] + " " + listYAxesObj.val[3]);
             console.log(listXAxesObj.val[2] + " " + listYAxesObj.val[2]);
             console.log(listXAxesObj.val[1] + " " + listYAxesObj.val[1]);
+            console.log(listXAxesObj.val[0] + " " + listYAxesObj.val[0]);
             changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 2);
             //changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 1);
             // changeCoordonates(width3, height3, width2, height2, currentValue3, currentValue2);
@@ -156,6 +158,7 @@ function startTheGame() {
             console.log(listXAxesObj.val[3] + " " + listYAxesObj.val[3]);
             console.log(listXAxesObj.val[2] + " " + listYAxesObj.val[2]);
             console.log(listXAxesObj.val[1] + " " + listYAxesObj.val[1]);
+            console.log(listXAxesObj.val[0] + " " + listYAxesObj.val[0]);
             changeCoordinates(listCoordObj, listXAxesObj, listYAxesObj, 1);
             // changeCoordonates(width2, height2, width1, height1, currentValue2, currentValue1);
             newRandomAsteroid(listCoordObj, listXAxesObj, listYAxesObj, initWidth, counter);
