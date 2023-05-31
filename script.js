199c411a13e0ad29a559fe0ef2c4cdebf693f110
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-31 14:54:48 +0300

    New

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function createObjects(width, height, valueCondition) {
     ctx.fill();
 }
 
-class Asteroid {
+class Object {
     constructor(width, height) {
         createObjects(width, height, 1);
     }
@@ -95,9 +95,9 @@ function startTheGame() {
     gameController(airplane, isDestroyed);
     let initWidth = 25, initHeight = 25;
     let counter = 0;
-    let asteroid = [];
+    let object = [];
     for (let i = 0; i * 100 + initWidth < canvasWidth; ++i) {
-        asteroid[i] = new Asteroid(i * 100 + initWidth, initHeight);
+        object[i] = new Object(i * 100 + initWidth, initHeight);
         ++counter;
     }
     let randomWidth = Math.floor(Math.random() * counter);
@@ -107,11 +107,11 @@ function startTheGame() {
     const listAxesYObj = {val: [25, 25, 25, 25]};
     idInterval = window.setInterval(function() {
         if (listAxesYObj.val[0] > 25) {
-            asteroid[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0] - 5);
+            object[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0] - 5);
         }
-        asteroid[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
-        asteroidGravity(asteroid, listCoordObj, listAxesXObj, listAxesYObj);
-        collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
+        object[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
+        objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj);
+        collision(idInterval, isDestroyed, airplane, object, score, listCoordObj, listAxesXObj, listAxesYObj);
         objectsTrajectory(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter, score);
         listAxesYObj.val[0] = listAxesYObj.val[0] + 5;
     }, 25);
@@ -121,23 +121,23 @@ function objectsTrajectory(listCoordObj, listAxesXObj, listAxesYObj, initWidth, 
     if (listAxesYObj.val[2] == 700) {
         ++score.val;
         changeCoordinates(listCoordObj, listAxesXObj, listAxesYObj, 3);
-        newRandomAsteroid(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
+        newRandomObject(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
     } else if (listAxesYObj.val[1] == 575) {
         changeCoordinates(listCoordObj, listAxesXObj, listAxesYObj, 2);
-        newRandomAsteroid(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
+        newRandomObject(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
     } else if (listAxesYObj.val[0] == 300) {
         changeCoordinates(listCoordObj, listAxesXObj, listAxesYObj, 1);
-        newRandomAsteroid(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
+        newRandomObject(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter);
     }
 }
 
-function collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj) {
+function collision(idInterval, isDestroyed, airplane, Object, score, listCoordObj, listAxesXObj, listAxesYObj) {
     if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
         listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
         && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
             listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
         isDestroyed.val = true;
-        GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
+        GameOver(isDestroyed, airplane, Object, score, listCoordObj, listAxesXObj, listAxesYObj);
         window.clearInterval(idInterval);
     }
 }
@@ -150,32 +150,32 @@ function changeCoordinates(listCoordObj, listAxesXObj, listAxesYObj, nrIteration
     }
 }
 
-function newRandomAsteroid(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter) {
+function newRandomObject(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter) {
     listCoordObj.val[0] = Math.floor(Math.random() * counter);
     listAxesXObj.val[0] = listCoordObj.val[0] * 100 + initWidth;
     listAxesYObj.val[0] = 25;
 }
 
-function asteroidGravity(asteroid, listCoordObj, listAxesXObj, listAxesYObj) {
+function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj) {
     for (let i = 1; i < 4; ++i) {
         if (listAxesXObj.val[i] > 0) {
             if (i < 3) {
                 listAxesYObj.val[i] = listAxesYObj.val[i] + 5;
-                asteroid[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
-                asteroid[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
+                object[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
+                object[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
             } else {
-                asteroid[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
-                asteroid[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
+                object[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
+                object[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
             }
         }
     }
 }
 
-function GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj) {
+function GameOver(isDestroyed, airplane, object, score, listCoordObj, listAxesXObj, listAxesYObj) {
     airplane.movePlaneToLeft(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
     airplane.movePlaneToRight(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
     for (let i = 0; i < 3; ++i) {
-        asteroid[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
+        object[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
     }
     ctx.fillStyle = "red";
     ctx.font = "48px Arial";
