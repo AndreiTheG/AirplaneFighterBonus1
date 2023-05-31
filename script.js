855e00e835fee5420cd8e2aae28714e7c5a57ff8
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-31 14:51:25 +0300

    NewCommit

--- script.js
+++ script.js
@@ -112,14 +112,6 @@ function startTheGame() {
         asteroid[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
         asteroidGravity(asteroid, listCoordObj, listAxesXObj, listAxesYObj);
         collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
-        // if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
-        //     listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
-        //     && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
-        //         listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
-        //     isDestroyed.val = true;
-        //     GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
-        //     window.clearInterval(idInterval);
-        // }
         objectsTrajectory(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter, score);
         listAxesYObj.val[0] = listAxesYObj.val[0] + 5;
     }, 25);
