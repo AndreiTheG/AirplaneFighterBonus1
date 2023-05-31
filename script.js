4b9bb1eaff5019e8ca4375898ccae39dc96e3b61
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-31 14:50:02 +0300

    New

--- script.js
+++ script.js
@@ -93,13 +93,6 @@ function startTheGame() {
     const airplane = new Airplane(squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
     const isDestroyed = {val: false};
     gameController(airplane, isDestroyed);
-    // addEventListener("keydown", (event) => {
-    //     if (event.code == 'ArrowRight' && squareXCoordinate.val <= canvasWidth - 55) {
-    //         airplane.movePlaneToRight(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
-    //     } else if (event.code == 'ArrowLeft' && squareXCoordinate.val >= 0) {
-    //         airplane.movePlaneToLeft(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
-    //     }
-    // })
     let initWidth = 25, initHeight = 25;
     let counter = 0;
     let asteroid = [];
@@ -118,15 +111,15 @@ function startTheGame() {
         }
         asteroid[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
         asteroidGravity(asteroid, listCoordObj, listAxesXObj, listAxesYObj);
-        //collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
-        if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
-            listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
-            && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
-                listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
-            isDestroyed.val = true;
-            GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
-            window.clearInterval(idInterval);
-        }
+        collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
+        // if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
+        //     listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
+        //     && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
+        //         listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
+        //     isDestroyed.val = true;
+        //     GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
+        //     window.clearInterval(idInterval);
+        // }
         objectsTrajectory(listCoordObj, listAxesXObj, listAxesYObj, initWidth, counter, score);
         listAxesYObj.val[0] = listAxesYObj.val[0] + 5;
     }, 25);
@@ -146,16 +139,16 @@ function objectsTrajectory(listCoordObj, listAxesXObj, listAxesYObj, initWidth, 
     }
 }
 
-// function collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj) {
-//     if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
-//         listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
-//         && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
-//             listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
-//         isDestroyed = true;
-//         GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
-//         window.clearInterval(idInterval);
-//     }
-// }
+function collision(idInterval, isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj) {
+    if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
+        listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
+        && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
+            listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
+        isDestroyed = true;
+        GameOver(isDestroyed, airplane, asteroid, score, listCoordObj, listAxesXObj, listAxesYObj);
+        window.clearInterval(idInterval);
+    }
+}
 
 function changeCoordinates(listCoordObj, listAxesXObj, listAxesYObj, nrIterations) {
     for (let i = nrIterations; i >= 1; --i) {
