6de3d1b66b139b12707739715c3bbb98410412a2
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-05-31 14:41:22 +0300

    New

--- script.js
+++ script.js
@@ -79,9 +79,7 @@ class Asteroid {
 
 startTheGame();
 
-function startTheGame() {
-    const airplane = new Airplane(squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
-    const isDestroyed = {val: false};
+function gameController(airplane, isDestroyed) {
     addEventListener("keydown", (event) => {
         if (event.code == 'ArrowRight' && squareXCoordinate.val <= canvasWidth - 55) {
             airplane.movePlaneToRight(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
@@ -89,6 +87,19 @@ function startTheGame() {
             airplane.movePlaneToLeft(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
         }
     })
+}
+
+function startTheGame() {
+    const airplane = new Airplane(squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
+    const isDestroyed = {val: false};
+    gameController(airplane, isDestroyed);
+    // addEventListener("keydown", (event) => {
+    //     if (event.code == 'ArrowRight' && squareXCoordinate.val <= canvasWidth - 55) {
+    //         airplane.movePlaneToRight(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
+    //     } else if (event.code == 'ArrowLeft' && squareXCoordinate.val >= 0) {
+    //         airplane.movePlaneToLeft(isDestroyed, squareWidth, squareHeight, squareXCoordinate, squareYCoordinate);
+    //     }
+    // })
     let initWidth = 25, initHeight = 25;
     let counter = 0;
     let asteroid = [];
