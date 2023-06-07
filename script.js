fae35f8bc8bc6579a9a143577f0fd42e0bff3f78
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-07 21:16:18 +0300

    New

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ function createObjects(width, height, valueCondition) {
     }
     if (valueCondition == 1) {
         ctx.fillStyle = "rgb(0, 0, 0)";
+        
     } else if (valueCondition == 2) {
         ctx.strokeStyle = "rgb(0, 0, 0)";
         ctx.fillStyle = "rgb(0, 0, 0)";
