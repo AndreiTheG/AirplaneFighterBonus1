52e5bf55eb32c3d4daca4d31f97d3ade2474f4d4
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-07 20:50:46 +0300

    New

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ class Airplane {
             }
             ctx.fillstyle = "rgb(0, 255, 0)";
             ctx.beginPath();
-            ctx.arc(squareXCoordinate, squareYCoordinate, 10, degToRad(0), degToRad(360), false);
+            ctx.arc(squareXCoordinate, squareYCoordinate - 40, 10, degToRad(0), degToRad(360), false);
             ctx.fill();
             console.log("Shooting......");
         }
