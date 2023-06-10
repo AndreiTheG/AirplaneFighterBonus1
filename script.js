fbfa6cbf8b442a17b782f3f7ec48235244e827fb
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-10 23:59:07 +0300

    New

--- script.js
+++ script.js
@@ -151,10 +151,10 @@ function startTheGame() {
                 }*/
                 if (isDestroyed.val == true) {
                     console.log("A disparut");
-                    ctx.fillStyle = "rgb(0, 0, 0)";
-                    ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    ctx.strokeStyle = "rgb(0, 0, 0)";
-                    ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
+                    //ctx.fillStyle = "rgb(0, 0, 0)";
+                    //ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
+                    //ctx.strokeStyle = "rgb(0, 0, 0)";
+                    //ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     //airplane.shoot(isDestroyed, fireXCoord, fireYCoord, shouted);
                     window.clearInterval(shooter);
                 }
