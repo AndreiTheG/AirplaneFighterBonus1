07f9fb8e1436e80fc3bb9ab23495c03ef6cb0ee6
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-11 14:19:34 +0300

    New

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ function startTheGame() {
                 ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                 ctx.strokeStyle = "rgb(0, 0, 0)";
                 ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);*/
-                if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
+                /*if ((squareXCoordinate.val - listAxesXObj.val[2] >= 0 && squareXCoordinate.val - listAxesXObj.val[2] < 20 || 
                     listAxesXObj.val[2] - squareXCoordinate.val >= 0 && listAxesXObj.val[2] - squareXCoordinate.val < 70) 
                     && (squareYCoordinate.val - listAxesYObj.val[2] >= 0 && squareYCoordinate.val - listAxesYObj.val[2] < 20 || 
                     listAxesYObj.val[2] - squareYCoordinate.val >= 0 && listAxesYObj.val[2] - squareYCoordinate.val < 70)) {
@@ -157,7 +157,7 @@ function startTheGame() {
                     ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     console.log("Este adevarat");
                     collision(idInterval, isDestroyed, airplane, object, score, listCoordObj, listAxesXObj, listAxesYObj);
-                }
+                }*/
                 if (isDestroyed.val == false) {
                     ctx.fillStyle = "rgb(0, 0, 0)";
                     ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
