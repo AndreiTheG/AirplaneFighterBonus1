481cbe86103cafdca073d854701f745cad39b021
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-24 19:40:13 +0300

    New

--- script.js
+++ script.js
@@ -196,21 +196,21 @@ function startTheGame() {
         }*/
         //console.log(collided.val[0] + ' ' + collided.val[1] + ' ' + collided.val[2]);
         object[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
-        if (collided.val[0] == false) {
+        //if (collided.val[0] == false) {
             objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
-        } else {
-            object[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0]);
-        }
-        if (collided.val[1] == false) {
-            objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
-        } else {
-            object[listCoordObj.val[1]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[1]);
-        }
-        if (collided.val[2] == false) {
-            objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
-        } else {
-            object[listCoordObj.val[2]].previousHeight(listAxesXObj.val[2], listAxesYObj.val[2]);
-        }
+        //} else {
+            //object[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0]);
+        //}
+        //if (collided.val[1] == false) {
+        //     objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
+        // } else {
+        //     object[listCoordObj.val[1]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[1]);
+        // }
+        // if (collided.val[2] == false) {
+        //     objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
+        // } else {
+        //     object[listCoordObj.val[2]].previousHeight(listAxesXObj.val[2], listAxesYObj.val[2]);
+        // }
         objectsHeights(listCoordObj, listAxesXObj, listAxesYObj, collided, initialWidth, counter, score, object);
         collision(idInterval, isDestroyed, airplane, score, listAxesXObj, listAxesYObj, collided);
         listAxesYObj.val[0] = listAxesYObj.val[0] + 5;
