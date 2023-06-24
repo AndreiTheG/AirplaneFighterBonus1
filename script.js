48d59b7f10076f868c3b9f9f60e1c49383a30e7c
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-24 19:41:14 +0300

    New

--- script.js
+++ script.js
@@ -196,9 +196,10 @@ function startTheGame() {
         }*/
         //console.log(collided.val[0] + ' ' + collided.val[1] + ' ' + collided.val[2]);
         object[listCoordObj.val[0]].fall(listAxesXObj.val[0], listAxesYObj.val[0]);
-        //if (collided.val[0] == false) {
+        if (collided.val[0] == false) {
             objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collided);
-        //} else {
+        } 
+        //else {
             //object[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0]);
         //}
         //if (collided.val[1] == false) {
