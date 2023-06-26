73b3f079fcff0762e103b6bfdda11388fb523c10
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-06-26 12:09:28 +0300

    New

--- script.js
+++ script.js
@@ -188,6 +188,9 @@ function startTheGame() {
             }, 40);
             shouted.val = false;
         }
+        if (collided.val[1] == true && collided.val[2] == true) {
+            console.log("Doua obiecte au fost distruse!");
+        }
         //console.log(collided.val[0] + "0 " + collided.val[1] + "1 " + collided.val[2] + "2 ");
         /*for (let i = 0; i < 3; i++) {
             if (collided.val[i] == true) {
