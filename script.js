c04736c146304597033883655562bbe2cee8fc3f
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-01 11:37:56 +0300

    New

--- script.js
+++ script.js
@@ -227,7 +227,10 @@ function objectsHeights(listCoordObj, listAxesXObj, listAxesYObj, collided, init
         createNewRandomObject(listCoordObj, listAxesXObj, listAxesYObj, initialWidth, collided, counter);
     }  else if (listAxesYObj.val[0] == 300) {
         for (let i = 3; i >= 2; ++i) {
-            
+            listAxesXObj.val[i] = listAxesXObj.val[i - 1];
+            listAxesYObj.val[i] = listAxesYObj.val[i - 1];
+            listCoordObj.val[i] = listCoordObj.val[i - 1];
+            collided.val[i] = collided.val[i - 1];
         }
         changeObjectCoordinates(listCoordObj, listAxesXObj, listAxesYObj, collided, 1);
         console.log("Partea 1 " + collided.val[0] + "0 " + collided.val[1] + "1 " + collided.val[2] + "2 ");
