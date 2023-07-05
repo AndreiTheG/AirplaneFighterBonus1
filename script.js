368c8a5de197e88aa38cdcc297e8a634a6acc951
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-05 15:08:44 +0300

    New

--- script.js
+++ script.js
@@ -143,6 +143,7 @@ function startTheGame() {
             object.val[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0] - 5);
         }
         if (shouted.val == true) {
+            console.log("Am tras!");
             const fireXCoord = {val: squareXCoordinate.val}, fireYCoord = {val: squareYCoordinate.val - 40};         
             const shooter = setInterval(function() {
                 if (isDestroyed.val == false) {
@@ -150,15 +151,15 @@ function startTheGame() {
                     ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     ctx.strokeStyle = "rgb(0, 0, 0)";
                     ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
+                    /*if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
                         ctx.fillStyle = "rgb(0, 0, 0)";
                         ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    } else if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
+                    }*/ //else if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
                         fireYCoord.val = fireYCoord.val - 5;
                         ctx.fillStyle = "rgb(255, 255, 0)";
                         ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                        
-                    }
+                    //}
                     if (fireXCoord.val >= listAxesXObj.val[0] && fireXCoord.val <= listAxesXObj.val[0] + 20
                         && fireYCoord.val >= listAxesYObj.val[0] && fireYCoord.val <= listAxesYObj.val[0] + 20
                         && collided.val[0] == false) {
