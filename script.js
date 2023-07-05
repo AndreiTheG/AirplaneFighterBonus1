3c6f9a8aba67aa7bd4a5f98ca9f5fe6ffdffc863
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-05 14:59:07 +0300

    New

--- script.js
+++ script.js
@@ -150,15 +150,15 @@ function startTheGame() {
                     ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     ctx.strokeStyle = "rgb(0, 0, 0)";
                     ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    //if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
+                    if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
+                        ctx.fillStyle = "rgb(255, 255, 0)";
+                        ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
+                    } else if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
                         fireYCoord.val = fireYCoord.val - 5;
                         ctx.fillStyle = "rgb(255, 255, 0)";
                         ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                        if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
-                            ctx.fillStyle = "rgb(255, 255, 0)";
-                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                        }
-                    //}
+                       
+                    }
                     if (fireXCoord.val >= listAxesXObj.val[0] && fireXCoord.val <= listAxesXObj.val[0] + 20
                         && fireYCoord.val >= listAxesYObj.val[0] && fireYCoord.val <= listAxesYObj.val[0] + 20
                         && collided.val[0] == false) {
