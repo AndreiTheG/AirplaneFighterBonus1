7e10fa909906d1e3c900f28847515e04a951fc95
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-05 14:52:03 +0300

    New

--- script.js
+++ script.js
@@ -147,16 +147,14 @@ function startTheGame() {
             const shooter = setInterval(function() {
                 if (isDestroyed.val == false) {
                     ctx.fillStyle = "rgb(0, 0, 0)";
-
-                    
                     ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     ctx.strokeStyle = "rgb(0, 0, 0)";
                     ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    /*if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
+                    //if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
                         fireYCoord.val = fireYCoord.val - 5;
                         ctx.fillStyle = "rgb(255, 255, 0)";
                         ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    }*/
+                    //}
                     if (fireXCoord.val >= listAxesXObj.val[0] && fireXCoord.val <= listAxesXObj.val[0] + 20
                         && fireYCoord.val >= listAxesYObj.val[0] && fireYCoord.val <= listAxesYObj.val[0] + 20
                         && collided.val[0] == false) {
