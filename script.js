3223d90a9ca2b1748f4f698cbc72cc66836cc80d
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-06 19:10:08 +0300

    New

--- script.js
+++ script.js
@@ -199,6 +199,8 @@ function startTheGame() {
                     } else if (fireXCoord.val >= listAxesXObj.val[1] && fireXCoord.val <= listAxesXObj.val[1] + 20
                         && fireYCoord.val >= listAxesYObj.val[1] && fireYCoord.val <= listAxesYObj.val[1] + 20
                         && collided.val[1] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[1] + " " + listAxesYObj.val[1]);
                             //object[listCoordObj.val[1]].previousHeight(listAxesXObj.val[1], listAxesYObj.val[1] - 5);
                             //object.val[listCoordObj.val[1]].previousHeight(listAxesXObj.val[1], listAxesYObj.val[1]);
@@ -208,6 +210,8 @@ function startTheGame() {
                     } else if (fireXCoord.val >= listAxesXObj.val[2] && fireXCoord.val <= listAxesXObj.val[2] + 20
                         && fireYCoord.val >= listAxesYObj.val[2] && fireYCoord.val <= listAxesYObj.val[2] + 20
                         && collided.val[2] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[2] + " " + listAxesYObj.val[2]);
                             //object.val[listCoordObj.val[2]].previousHeight(listAxesXObj.val[2], listAxesYObj.val[2]);
                             collided.val[2] = true;
@@ -216,6 +220,8 @@ function startTheGame() {
                     } else if (fireXCoord.val >= listAxesXObj.val[0] - 20 && fireXCoord.val <= listAxesXObj.val[0]
                         && fireYCoord.val >= listAxesYObj.val[0] && fireYCoord.val <= listAxesYObj.val[0] + 20
                         && collided.val[0] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[0] + " " + listAxesYObj.val[0]);
                             //object.val[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0]);
                             collided.val[0] = true;
@@ -224,6 +230,8 @@ function startTheGame() {
                     } else if (fireXCoord.val >= listAxesXObj.val[1] - 20 && fireXCoord.val <= listAxesXObj.val[1]
                         && fireYCoord.val >= listAxesYObj.val[1] && fireYCoord.val <= listAxesYObj.val[1] + 20
                         && collided.val[1] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[1] + " " + listAxesYObj.val[1]);
                             //object.val[listCoordObj.val[1]].previousHeight(listAxesXObj.val[1], listAxesYObj.val[1]);
                             collided.val[1] = true;
@@ -232,6 +240,8 @@ function startTheGame() {
                     } else if (fireXCoord.val >= listAxesXObj.val[2] - 20 && fireXCoord.val <= listAxesXObj.val[2]
                         && fireYCoord.val >= listAxesYObj.val[2] && fireYCoord.val <= listAxesYObj.val[2] + 20
                         && collided.val[2] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[2] + " " + listAxesYObj.val[2]);
                             //object.val[listCoordObj.val[2]].previousHeight(listAxesXObj.val[2], listAxesYObj.val[2]);
                             collided.val[2] = true;
