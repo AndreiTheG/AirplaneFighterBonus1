8b4ae4ee500623f7e5c86205f8620abb0c89d471
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-06 19:07:05 +0300

    New

--- script.js
+++ script.js
@@ -178,17 +178,19 @@ function startTheGame() {
                         //ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                         //clearInterval(shooter);
                     //}
-                    if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
+                    /*if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
                         
                         ctx.fillStyle = "rgb(0, 0, 0)";
                         ctx.fillRect(fireBalls[fireXCoord.val], fireYCoord.val, 10, 20);
-                    } //else if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
+                    }*/ //else if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
                     
                        
                     //}
                     if (fireXCoord.val >= listAxesXObj.val[0] && fireXCoord.val <= listAxesXObj.val[0] + 20
                         && fireYCoord.val >= listAxesYObj.val[0] && fireYCoord.val <= listAxesYObj.val[0] + 20
                         && collided.val[0] == false) {
+                            ctx.fillStyle = "rgb(0, 0, 0)";
+                            ctx.fillRect(fireBalls[fireXCoord.val], fireYCoord.val, 10, 20);
                             //console.log(listAxesXObj.val[0] + " " + listAxesYObj.val[0]);
                             //object.val[listCoordObj.val[0]].previousHeight(listAxesXObj.val[0], listAxesYObj.val[0]);
                             collided.val[0] = true;
