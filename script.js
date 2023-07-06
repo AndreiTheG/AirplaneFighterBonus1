5655139dced80cee5e56abbc1143f084da73c9c0
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-06 20:02:14 +0300

    New

--- script.js
+++ script.js
@@ -97,14 +97,14 @@ class FireBalls {
     }
 
     shooter(fireXCoord, fireYCoord) {
-        fireYCoord = fireYCoord - 5;
+        fireYCoord.val = fireYCoord.val - 5;
         ctx.fillStyle = "rgb(255, 255, 0)";
-        ctx.fillRect(fireXCoord, fireYCoord, 10, 20);
+        ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
     }
 
     disappear(fireXCoord, fireYCoord) {
         ctx.fillStyle = "rgb(0, 0, 0)";
-        ctx.fillRect(fireXCoord, fireYCoord, 10, 20);
+        ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
     }
 
 }
