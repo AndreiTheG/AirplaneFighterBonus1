0a2df7f833b8e59fc170b63f5ed7d2f04eaba54a
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-07 15:47:11 +0300

    NEw

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function startTheGame() {
                     // ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                     // ctx.strokeStyle = "rgb(0, 0, 0)";
                     // ctx.strokeRect(fireXCoord.val, fireYCoord.val, 10, 20);
-                    if (collided.val[0] == true && collided.val[1] == true && collided.val[2] == true) {
+                    if (collided.val[0] == false && collided.val[1] == false && collided.val[2] == false) {
                         ctx.fillStyle = "rgb(0, 0, 0)";
                         ctx.fillRect(fireXCoord.val, fireYCoord.val, 10, 20);
                         ctx.strokeStyle = "rgb(0, 0, 0)";
