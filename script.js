fea42534bdca40b1c52a43c342230ecfe803b497
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-07 14:09:41 +0300

    New

--- script.js
+++ script.js
@@ -203,9 +203,9 @@ function startTheGame() {
                     //}
                     for (let i = 0; i < 400; ++i) {
                         fireBalls.val[fireXCoord.val].shooter(fireXCoord, fireYCoord);
-                        if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
+                        /*if (collided.val[0] == true || collided.val[1] == true || collided.val[2] == true) {
                             fireBalls.val[fireXCoord.val].disappear(fireXCoord, fireYCoord);
-                        }
+                        }*/
                     }
                     //    ctx.fillStyle = "rgb(0, 0, 0)";
                     //    ctx.fillRect(fireBalls[fireXCoord.val], fireYCoord.val, 10, 20);
