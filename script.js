33251eeb6ede7d268940970b200c07bee2e32bbc
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-09 16:03:45 +0300

    New

--- script.js
+++ script.js
@@ -254,16 +254,16 @@ function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collide
                         console.log("Conditie indeplinita");
                     }
                 }
-                else if (collided.val[i] == true && listAxesXObj.val[i] != listAxesXObj.val[i - 1]) {
+                /*else if (collided.val[i] == true && listAxesXObj.val[i] != listAxesXObj.val[i - 1]) {
                     console.log("Fals!");
                     object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                     object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
-                } else {
+                }*/ else {
                     object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                     object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
-                    listCoordObj.val[i] = listCoordObj.val[i - 1];
-                    listAxesXObj.val[i] = listAxesXObj.val[i - 1];
-                    listAxesYObj.val[i] = listAxesYObj.val[i - 1];
+                    //listCoordObj.val[i] = listCoordObj.val[i - 1];
+                    //listAxesXObj.val[i] = listAxesXObj.val[i - 1];
+                    //listAxesYObj.val[i] = listAxesYObj.val[i - 1];
                 }
             } else {
                 object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
