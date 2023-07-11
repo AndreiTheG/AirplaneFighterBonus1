5e1ce46ebbc081feeab7522e39fb8a1a9ed5413c
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-11 18:48:41 +0300

    New

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collide
     for (let i = 1; i < 4; ++i) {
         if (listAxesXObj.val[i] > 0) {
             //if (collided.val[i] == false) {
-                //if (i < 3) {
+                if (i < 3) {
                     //object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                     //object.val[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
                     //console.log(i + " " + listAxesXObj.val[i] + " " + listAxesYObj.val[i]);
@@ -269,7 +269,7 @@ function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collide
                         //object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                         //object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                     }*/
-               // } else {
+                } //else {
                     //object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                 //    object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                 //}
