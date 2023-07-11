39a4f296ba69fb55043d532bfd8c9223e02691d0
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-11 19:13:16 +0300

    New

--- script.js
+++ script.js
@@ -256,13 +256,12 @@ function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collide
                         listAxesYObj.val[i] = listAxesYObj.val[i] + 5;
                         object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                         object.val[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
-                        if (collided.val[i] == true) {
-                            object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
-                        }
                         //if (listAxesXObj.val[i] != listAxesXObj.val[i + 1]) {
                         // object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                         // object.val[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
                         //}
+                    } else if (collided.val[i] == true) {
+                        object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                     }/* else if (collided.val[i] == true && listAxesXObj.val[i] != listAxesXObj.val[i - 1]) {
                         object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                     } else {
