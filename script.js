56f21d3a667b846a415a490054f4cef18ef1fe1e
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-07-11 17:05:47 +0300

    New

--- script.js
+++ script.js
@@ -254,9 +254,10 @@ function objectGravity(object, listCoordObj, listAxesXObj, listAxesYObj, collide
                         //if (listAxesXObj.val[i] != listAxesXObj.val[i + 1]) {
                             
                         //if (listAxesXObj.val[i] != listAxesXObj.val[i + 1]) {
-                            listAxesYObj.val[i] = listAxesYObj.val[i] + 5;
+                            
                             object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i] - 5);
                             object.val[listCoordObj.val[i]].fall(listAxesXObj.val[i], listAxesYObj.val[i]);
+                            listAxesYObj.val[i] = listAxesYObj.val[i] + 5;
                     } if (collided.val[i] == true && listAxesXObj.val[i] != listAxesXObj.val[i - 1]) {
                         object.val[listCoordObj.val[i]].previousHeight(listAxesXObj.val[i], listAxesYObj.val[i]);
                     } /*else {
